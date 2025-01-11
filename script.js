cc221d9a4539d969594b24989c116a9b762d35c9
NoixVerte <65561735+NoixVerte@users.noreply.github.com>
2025-01-11 14:44:56 +0200

    Make Etch-a-Sketch

---
+++ script.js
@@ -0,0 +1,36 @@
+const wrapper = document.querySelector("#wrapper");
+const button = document.querySelector("button");
+const defaultSquareNumber = 16;
+
+generateGrid(defaultSquareNumber);
+
+button.addEventListener("click", () => {
+    let input;
+    do {
+        input = prompt("Write the number of squares you'd like to have per side");
+        if (input == 0 || input > 100) alert("The number needs to be between 1 and 100!");
+    } 
+    while (input == 0 || input > 100)
+    wipeGrid();
+    generateGrid(input);
+})
+
+function generateGrid(squareNumber) {
+    for (let i = 0; i < squareNumber*squareNumber; i++) {
+        let square = document.createElement("div");
+        square.classList.add("squares");
+        square.style.width = wrapper.clientWidth/squareNumber + "px";
+        square.style.height = wrapper.clientHeight/squareNumber + "px";
+        square.addEventListener("mouseover", () => {
+            square.style.background = "gray";
+        })
+        wrapper.appendChild(square);
+        console.log("Square created!");
+    }
+}
+
+function wipeGrid() {
+    while (wrapper.hasChildNodes()) {
+        wrapper.firstChild.remove();
+    }
+}
